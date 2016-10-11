6f27a585b48ca96fd722dd48b99c0db7257aba22
HeleneGuillemain <heleneguillemain@gmail.com>
2016-10-11 22:16:17 +0200

    reTest

---
+++ ApplicationNET/Models/Class Metier/CHIMPANZE.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApplicationNET.Models.Class_Metier
+{
+    public class CHIMPANZE
+    {
+        public int ChimpanzeId { get; set; }
+        public string ChimpanzeNom { get; set; }
+    }
+}
