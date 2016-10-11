028dc0113e40f5a4eaacbd6263a6ef6bd8641329
HeleneGuillemain <heleneguillemain@gmail.com>
2016-10-11 21:25:06 +0200

    Ajout classe test

---
+++ ApplicationNET/Models/Class Metier/Class_test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApplicationNET.Models.Class_Metier
+{
+    public class Class_test
+    {
+        public int testId { get; set; }
+        public string testNom { get; set; }
+    }
+}
